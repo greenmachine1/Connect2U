c55d7a21bf148723f5a9db2553a462519b591b89
Cory Green <scary_penguins05@hotmail.com>
2014-11-16 01:18:50 -0800

    added new storyboards and corresponding classes

---
+++ Connect2U/Connect2U/LoggedIn.swift
@@ -0,0 +1,35 @@
+//
+//  LoggedIn.swift
+//  Connect2U
+//
+//  Created by Cory Green on 11/16/14.
+//  Copyright (c) 2014 com.Cory. All rights reserved.
+//
+
+import UIKit
+
+class LoggedIn: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        println("Logged in")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
