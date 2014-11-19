d36964b097b0e23afb12fd87763c4539f4963018
Cory Green <scary_penguins05@hotmail.com>
2014-11-19 01:46:52 -0800

    making small tweaks in my overall design

--- Connect2U/Connect2U/AccountCreationViewController.swift
+++ Connect2U/Connect2U/AccountCreationViewController.swift
@@ -10,15 +10,14 @@ import UIKit
 
 class AccountCreationViewController: UIViewController {
 
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor(red: 0.431, green: 0.808, blue: 0.933, alpha: 1.0)
-        
+        self.setColors()
 
-        
-
-        println("Account Creation")
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,18 +27,31 @@ class AccountCreationViewController: UIViewController {
     
     
     
-    
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // on signup button //
+    @IBAction func signUpOnClick(sender: AnyObject) {
+        
+        // should take the user to set up their age, picture, gender, and interests //
+        
+        
+        
     }
-    */
+    
+    
+    
+
+    // setting colors for the view //
+    func setColors(){
+        
+        var whiteColor:UIColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        var lightBlueColor:UIColor = UIColor(red: 0.431, green: 0.808, blue: 0.933, alpha: 1.0)
+        var greenColor:UIColor = UIColor(red: 0.192, green: 0.733, blue: 0.855, alpha: 1.0)
+        var darkGreenColor:UIColor = UIColor(red: 0.075, green: 0.467, blue: 0.557, alpha: 1.0)
+        
+        self.view.backgroundColor = lightBlueColor
+        
+        signUpButton.backgroundColor = greenColor
+        signUpButton.tintColor = whiteColor
+        
+    }
 
 }
