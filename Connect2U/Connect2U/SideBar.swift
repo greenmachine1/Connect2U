e3c7322e06aa34bef2b24347da2363a2d5e4df8d
Cory Green <scary_penguins05@hotmail.com>
2014-11-19 22:52:31 -0800

    more work on the sidebar

--- Connect2U/Connect2U/SideBar.swift
+++ Connect2U/Connect2U/SideBar.swift
@@ -20,6 +20,87 @@ import UIKit
 }
 
 class SideBar: NSObject, FriendsTableViewControllerDelegate {
+    
+    // creating info for the sidebar
+    let widthOfBar:CGFloat = 150.0
+    let sideBarTopInset:CGFloat = 64.0
+    let sideBarContainerView:UIView = UIView()
+    let sideBarTableViewController:FriendsTableViewController = FriendsTableViewController()
+    let origin:UIView!
+    var delegate:SideBarDelegate?
+    var sideBarOpen:Bool = false
+    
+    
+    override init(){
+        super.init()
+    }
+    
+    init(callingView:UIView, items:Array<String>) {
+        super.init()
+        
+        // setting the calling view //
+        origin = callingView
+        
+        // sending the data from the origin to the FriendsTableViewController //
+        sideBarTableViewController.friendsData = items
+        
+        
+        // making the actual sidebar //
+        sideBarSetup()
+        
+        // setting up the gestures associated with this bar //
+        // to close //
+        let gestureRecognizer:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "handleSwipe:")
+        gestureRecognizer.direction = UISwipeGestureRecognizerDirection.Right
+        origin.addGestureRecognizer(gestureRecognizer)
+        
+        
+        // to expose //
+        let hideGesture:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "handleSwipe:")
+        hideGesture.direction = UISwipeGestureRecognizerDirection.Left
+        origin.addGestureRecognizer(hideGesture)
+        
+        
+    }
+    
+    func sideBarSetup(){
+        
+        // creation of the dimensions of the sidebar //
+        sideBarContainerView.frame = CGRectMake(-widthOfBar - 1, origin.frame.origin.y, widthOfBar, origin.frame.size.height)
+        
+        sideBarContainerView.backgroundColor = UIColor.clearColor()
+        sideBarContainerView.clipsToBounds = false
+        
+        // adding the sidebarcontainerview to the view //
+        origin.addSubview(sideBarContainerView)
+        
+        
+        
+        // creating a blurred background //
+        let blurView:UIVisualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: UIBlurEffectStyle.Light))
+        blurView.frame = sideBarContainerView.bounds
+        sideBarContainerView.addSubview(blurView)
+        
+        sideBarTableViewController.delegate = self
+        sideBarTableViewController.tableView.frame = sideBarContainerView.bounds
+        sideBarTableViewController.tableView.clipsToBounds = false
+        sideBarTableViewController.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
+        sideBarTableViewController.tableView.backgroundColor = UIColor.clearColor()
+        sideBarTableViewController.tableView.scrollsToTop = false
+        sideBarTableViewController.tableView.contentInset = UIEdgeInsetsMake(sideBarTopInset, 0.0, 0.0, 0.0)
+        
+        
+        // reloading the data within //
+        sideBarTableViewController.tableView.reloadData()
+        
+        
+        // adding the table view //
+        sideBarContainerView.addSubview(sideBarTableViewController.tableView)
+        
+    }
+    
+    
+    // swipe gestures //
 
     
     // this is a required delegate method that goes along with //
